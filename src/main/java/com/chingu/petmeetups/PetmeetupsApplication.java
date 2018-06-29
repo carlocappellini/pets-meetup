860783ca6ed9d8b7911525de135060604742a01c
Bolimbo <carlo.cappellini@yahoo.com>
2018-06-28 22:11:33 -0500

    using mysql5, warning messages jpa.open-in-view excluded

--- src/main/java/com/chingu/petmeetups/PetmeetupsApplication.java
+++ src/main/java/com/chingu/petmeetups/PetmeetupsApplication.java
@@ -3,6 +3,7 @@ package com.chingu.petmeetups;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication //(exclude = {SecurityAutoConfiguration.class })
 public class PetmeetupsApplication  {
     public static void main(String[] args) {
